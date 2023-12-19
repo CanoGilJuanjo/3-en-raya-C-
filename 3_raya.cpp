fe3d93cc823cff771317c295accaafb4ae0c37d8
Juan Jose Cano Gil <149374608+CanoGilJuanjo@users.noreply.github.com>
2023-12-19 13:02:31 +0100

    Cambios realizados para optimizar y limpiar codigo
    Se ha modificado el código, reseteándolo desde cero para estructurarlo de manera más clara y evitando la redundancia de código. Se han añadido funciones para el control del juego y se han añadido casos de victoria para detener el juego.

--- 3_raya.cpp
+++ 3_raya.cpp
@@ -1,314 +1,300 @@
 #include <iostream>
 #include <array>
-#include <exception>
+#include <math.h>
+#include <cstdlib>
+
 
 using namespace std;
 
-// ACLARACION: El codigo fue compilado antes de cambiar los mensajes de error
+//Variables constantes y matriz      Const variables and array matrix
+const int DIMENSION_MATRIZ = 3; 
+typedef array<int, DIMENSION_MATRIZ>Vfila;
+typedef array<Vfila,DIMENSION_MATRIZ>Vmatriz;
 
-int main()
-{
-    //Creacion de variables
-    typedef array<int,3>Vector;
-    typedef array<Vector,3>Tablero;
-    
-    Tablero tablero;
-    
-    for(int i = 0; i<3;i++)
-    {
-        for(int j = 0; j<3;j++)
-        {
-            tablero[i][j] = 0;
-        }
-    }
-    
-    bool victoria = false;
-    bool jugador = true;    //<- controlamos el jugador que ha ganado
-    bool empate = true;
-    do{
-        //  Representacion
-        empate = true;
-        unsigned con = 0;
-        double pos;
-        system("cls");
-        for(int i = 0; i<3;i++)
-        {
-            for(int j = 0; j<3;j++)
-            {
-                if(tablero[i][j] == 0)
-                {
-                    cout<<"[ "<<con+1<<" ] ";
-                }else if(tablero[i][j] == 1)
-                {
-                    cout<<"[ X ] ";
-                }else if(tablero[i][j] == 2)
-                {
-                    cout<<"[ O ] ";
-                }
-                con++;
-            }
-            cout<<"\n";
-        }
-        
-        do
-        {
-            cout<<"-Que posicion desea Jugador 1?"<<endl;
-            cout<<"opcion: ";
-            cin>>pos;  
-            if(pos>9 || pos<1)
-            {
-                cout<<"Error"<<endl;
-            }
-        }while(pos>9 || pos<1);
-        
-         if(pos<=3)
-        {
-            pos--;
-            if(tablero[0][pos] != 0)
-            {
-                cout<<"Error"<<endl;
-            }else
-            {
-                tablero[0][pos] = 1;
-            }
-        }else if(pos<=6)
-        {
-            pos -= 4;
-            if(tablero[1][pos] != 0)
-            {
-                cout<<"Error"<<endl;
-            }else
-            {
-                tablero[1][pos] = 1;
-            }
-        }else if(pos<=9)
-        {
-            pos -= 7;
-            if(tablero[2][pos] == 0)
-            {
-                tablero[2][pos] = 1;
-            }
-        }
-        
-        for(int i = 0;i<3;i++)
-        {
-            for(int j = 0;j<3;j++)
-            {
-                if(tablero[i][j] == 0)
-                {
-                    empate = false;
-                }
-            }
-        }
+//Referencia de funciones           function references
+void interpretarPosicion(int posicion, int &i ,int &j);
+void representar(Vmatriz &matriz);
+bool ganador1(Vmatriz matriz);
+bool ganador2(Vmatriz matriz);
+bool empate(Vmatriz matriz);
 
-        //  Representacion
-        system("cls");
-        con = 0;
-        for(int i = 0; i<3;i++)
-        {
-            for(int j = 0; j<3;j++)
-            {
-                if(tablero[i][j] == 0)
-                {
-                    cout<<"[ "<<con+1<<" ] ";
-                }else if(tablero[i][j] == 1)
-                {
-                    cout<<"[ X ] ";
-                }else if(tablero[i][j] == 2)
-                {
-                    cout<<"[ O ] ";
-                }
-                con++;
-            }
-            cout<<"\n";
-        }
-        /*
-            Comprobamos las victorias, si se cumple alguna de las siguientes condiciones, se acabo la partida
-            Feature: Optimizar la condicion para desarrollarlo en menos lineas
-        */
-        if(
-	    (tablero[0][0] != 0 && tablero[0][0] == tablero[0][1] && tablero[0][1] == tablero[0][2])||
-        (tablero[1][0] != 0 && tablero[1][0] == tablero[1][1] && tablero[1][1] == tablero[1][2])||
-        (tablero[2][0] != 0 && tablero[2][0] == tablero[2][1] && tablero[2][1] == tablero[2][2])||
-	    (tablero[0][0] != 0 && tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2])||
-        (tablero[0][2] != 0 && tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0])||
-        (tablero[0][0] != 0 && tablero[0][0] == tablero[1][0] && tablero[1][0] == tablero[2][0])||
-        (tablero[0][1] != 0 && tablero[0][1] == tablero[1][1] && tablero[1][1] == tablero[2][1])||
-        (tablero[0][2] != 0 && tablero[0][2] == tablero[1][2] && tablero[1][2] == tablero[2][2]))
-        {
-            victoria = true;
-        }
-        
-        // comprobamos empate
-        for(int i = 0;i<3;i++)
-        {
-            for(int j = 0;j<3;j++)
-            {
-                if(empate && tablero[i][j] == 0)
-                {
-                    empate = false;
-                }
-            }
-        }
-        
-        if(empate)
-        {
-            goto exit;
-        }
+//Main
+int main(){
+    //Creamos la matriz             Create the matrix
+    Vmatriz matriz;
 
-        if(victoria)
-        {
-            jugador = false;
-            goto exit;
-        }
-        
-        //Intento de control de excepciones
-        do
-        {
-            cout<<"-Que posicion desea Jugador 2?"<<endl;
-            cout<<"opcion: ";
-            try{
-                cin>>pos;
-            }catch (runtime_error& e){
-                throw e;
-            }
-            if (pos>9 || pos<1){
-                cout<<"Error"<<endl;
-            }
-        }while(pos>9 || pos<1);
-        
-        if(pos<=3)
-        {
-            pos--;
-            if(tablero[0][pos] != 0)
-            {
-                cout<<"Error"<<endl;
-            }else
-            {
-                tablero[0][pos] = 2;
-            }
-        }else if(pos<=6)
-        {
-            pos -= 4;
-            if(tablero[1][pos] != 0)
-            {
-                cout<<"Error"<<endl;
-            }else
-            {
-                tablero[1][pos] = 2;
-            }
-        }else if(pos<=9)
-        {
-            pos -= 7;
-            if(tablero[2][pos] != 0)
-            {
-                cout<<"Error"<<endl;
-            }else
-            {
-                tablero[2][pos] = 2;
-            }
-        }
-
-        /*
-            Comprobamos las victorias, si se cumple alguna de las siguientes condiciones, se acabo la partida
-        */
-       if(
-	    (tablero[0][0] != 0 && tablero[0][0] == tablero[0][1] && tablero[0][1] == tablero[0][2])||
-        (tablero[1][0] != 0 && tablero[1][0] == tablero[1][1] && tablero[1][1] == tablero[1][2])||
-        (tablero[2][0] != 0 && tablero[2][0] == tablero[2][1] && tablero[2][1] == tablero[2][2])||
-        (tablero[0][0] != 0 && tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2])||
-        (tablero[0][2] != 0 && tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0])||
-        (tablero[0][0] != 0 && tablero[0][0] == tablero[1][0] && tablero[1][0] == tablero[2][0])||
-        (tablero[0][1] != 0 && tablero[0][1] == tablero[1][1] && tablero[1][1] == tablero[2][1])||
-        (tablero[0][2] != 0 && tablero[0][2] == tablero[1][2] && tablero[1][2] == tablero[2][2]))
-        {
-            victoria = true;
-        }
-
-        // comprobamos empate
-        for(int i = 0;i<3;i++)
-        {
-            for(int j = 0;j<3;j++)
-            {
-                if(empate && tablero[i][j] == 0)
-                {
-                    empate = false;
-                }
-            }
-        }
-        
-        if(empate)
-        {
-            goto exit;
-        }
-
-    }while(!victoria);
-
-    //Salida, usada para el caso de la victoria del primer jugador, si no se usa pedira al segundo jugador la posicion aunque ya haya ganado
-    exit:
-    unsigned con = 0;
+    //Inicializamos la matriz con 0      Set 0 as default value
     system("cls");
-
-    if(jugador && !empate)     //Caso de que gane el jugador 2
-    {
-        for(int i = 0; i<3;i++)
-        {
-            for(int j = 0; j<3;j++)
-            {
-                if(tablero[i][j] == 0)
-                {
-                    cout<<"[ "<<" "<<" ] ";
-                }else if(tablero[i][j] == 1)
-                {
-                    cout<<"[   ] ";
-                }else if(tablero[i][j] == 2)
-                {
-                    cout<<"[ O ] ";
-                }
-                con++;
-            }
-            cout<<"\n";
-        }
-    }else if(!jugador && !empate)
-    {
-        for(int i = 0; i<3;i++)
-        {
-            for(int j = 0; j<3;j++)
-            {
-                if(tablero[i][j] == 0)
-                {
-                    cout<<"[   ] ";
-                }else if(tablero[i][j] == 1)
-                {
-                    cout<<"[ X ] ";
-                }else if(tablero[i][j] == 2)
-                {
-                    cout<<"[   ] ";
-                }
-                con++;
-            }
-            cout<<"\n";
-        }
-    }else
-    {
-        for(int i = 0; i<3;i++)
-        {
-            for(int j = 0; j<3;j++)
-            {
-                if(tablero[i][j] == 1)
-                {
-                    cout<<"[ X ] ";
-                }else if(tablero[i][j] == 2)
-                {
-                    cout<<"[ O ] ";
-                }
-                con++;
-            }
-            cout<<"\n";
+    for(int i = 0;i<DIMENSION_MATRIZ;i++){
+        for(int j = 0;j<DIMENSION_MATRIZ;j++){
+            matriz[i][j] = 0;
         }
     }
+
+    representar(matriz);
+
+    //Variables de control de victoria          win variables control       
+    bool finJ1 = false;
+    bool finJ2 = false;
+    bool win = false;
+
+    //Partida
+    do
+    {
+        //Turno J1
+        while (!finJ1)
+        {
+            int posicionSolicitada = 0;
+            
+            cout<<"Turno: Jugador 1"<<endl;
+            while(!(cin >> posicionSolicitada) || posicionSolicitada<1 || posicionSolicitada > pow(DIMENSION_MATRIZ,2)){            
+                cin.clear();
+                cin.ignore(1000, '\n');
+                cout<<"Posiciones erroneas, intentelo de nuevo"<<endl;
+            }
+
+            int i = 0, j = 0;
+            interpretarPosicion(posicionSolicitada-1,i,j);
+            if(matriz[i][j] == 0){
+                matriz[i][j] = 1;
+                finJ1 = true;
+            }
+            system("cls");
+            representar(matriz);
+            win = ganador1(matriz);
+            if(win){
+                cout<<"Victoria Jugador 1"<<endl;
+                system("pause");
+            }
+        }
+        finJ1 = false;
+
+        //Comprobamos el caso de empate
+        if(!win){
+            win = empate(matriz);
+            if(win){
+                system("cls");
+                cout<<"Empate"<<endl;
+                system("pause");
+            }
+        }
+
+        if(win){
+            goto exit;
+        }
+
+        //Turno J2
+        while (!finJ2)
+        {
+            int posicionSolicitada = 0;
+            
+            cout<<"Turno: Jugador 2"<<endl;
+            while(!(cin >> posicionSolicitada) || posicionSolicitada<1 || posicionSolicitada > pow(DIMENSION_MATRIZ,2)){            
+                cin.clear();
+                cin.ignore(1000, '\n');
+                cout<<"Posiciones erroneas, intentelo de nuevo"<<endl;
+            }
+
+            int i = 0, j = 0;
+            interpretarPosicion(posicionSolicitada-1,i,j);
+            if(matriz[i][j] == 0){
+                matriz[i][j] = 2;
+                finJ2 = true;
+            }
+            system("cls");
+            representar(matriz);
+            win = ganador2(matriz);
+            if(win){
+                cout<<"Victoria Jugador 2"<<endl;
+                system("pause");
+            }
+        }
+        finJ2 = false;
+
+        //Comprobamos el caso de empate
+        if(!win){
+            win = empate(matriz);
+            if(win){
+                system("cls");
+                cout<<"Empate"<<endl;
+                system("pause");
+            }
+        }
+    }while (!win);
     
-    cout<<"Fin del Juego"<<endl;
-    system("pause");
+    exit:
     return 0;
+}
+
+//Funciones         Funtions
+
+void interpretarPosicion(int posicion, int &i ,int &j){
+    if(posicion<=3){
+        i = 0, j = posicion;
+    }else if(posicion <= 6){
+        i = 1, j = posicion-3;
+    }else{
+        i = 2, j = posicion-6;
+    }
+}
+
+/*
+    Para el jugador 1 tenemos la X
+    Para el jugador 2 tenemos el O
+    Si es 0 el valor sera vacio y no se representa
+*/
+void representar(Vmatriz &matriz){
+    int contador = 1;
+    for(int i = 0;i<DIMENSION_MATRIZ;i++){
+        for (int j = 0; j < DIMENSION_MATRIZ; j++){
+            cout<<"[";
+            if(matriz[i][j] == 0){
+                cout<<contador;
+            }else if(matriz[i][j] == 1){
+                cout<<"X";
+            }else if(matriz[i][j] == 2){
+                cout<<"O";
+            }else{
+                cout<<"E";
+            }
+            cout<<"] ";
+            contador++;
+        }
+        cout<<endl;
+    }
+}
+
+//Win control
+bool ganador1(Vmatriz matriz){
+    int contador = 0;
+    
+    //Diagonal principal
+    for (int i = 0; i < DIMENSION_MATRIZ; i++)
+    {
+        if(matriz[i][i] == 1){
+            contador++;
+        }
+        
+    }
+    if(contador == 3){
+        return true;
+    }
+
+    //Diagonal inversa
+    if(matriz[2][0] == 1 && matriz[1][1] == 1 && matriz[0][2] == 1){
+        return true;
+    }
+
+    //Columnas
+    contador = 0;
+    for (int i = 0; i < DIMENSION_MATRIZ; i++)
+    {
+        for (int j = 0; j < DIMENSION_MATRIZ; j++)
+        {
+            if(matriz[j][i] == 1){
+                contador++;
+            }
+        }
+        if(contador == 3){
+            return true;
+        }else{
+            contador = 0;
+        }
+        
+    }
+    
+    //Filas
+    for (int i = 0; i < DIMENSION_MATRIZ; i++)
+    {
+        for (int j = 0; j < DIMENSION_MATRIZ; j++)
+        {
+            if(matriz[i][j] == 1){
+                contador++;
+            }
+        }
+        if(contador == 3){
+            return true;
+        }else{
+            contador = 0;
+        }
+        
+    }
+
+    return false;
+}
+
+bool ganador2(Vmatriz matriz){
+    int contador = 0;
+    
+    //Diagonal principal
+    for (int i = 0; i < DIMENSION_MATRIZ; i++)
+    {
+        if(matriz[i][i] == 1){
+            contador++;
+        }
+    }
+    if(contador == 3){
+        return true;
+    }
+
+    //Diagonal inversa          
+    if(matriz[2][0] == 2 && matriz[1][1] == 2 && matriz[0][2] == 2){
+        return true;
+    }
+
+    //Columnas      columns
+    contador = 0;
+    for (int i = 0; i < DIMENSION_MATRIZ; i++)
+    {
+        for (int j = 0; j < DIMENSION_MATRIZ; j++)
+        {
+            if(matriz[j][i] == 2){
+                contador++;
+            }
+        }
+        if(contador == 3){
+            return true;
+        }else{
+            contador = 0;
+        }
+        
+    }
+    
+    //Filas         rows
+    for (int i = 0; i < DIMENSION_MATRIZ; i++)
+    {
+        for (int j = 0; j < DIMENSION_MATRIZ; j++)
+        {
+            if(matriz[i][j] == 2){
+                contador++;
+            }
+        }
+        if(contador == 3){
+            return true;
+        }else{
+            contador = 0;
+        }
+        
+    }
+
+    return false;
+}
+
+bool empate(Vmatriz matriz){
+    int contador = 0;
+    for (int i = 0; i < DIMENSION_MATRIZ; i++)
+    {
+        for (int j = 0; j < DIMENSION_MATRIZ && matriz[i][j] != 0; j++)
+        {
+            if(matriz[i][j] != 0){
+                contador++;
+            }
+        }
+    }
+    if(contador == pow(DIMENSION_MATRIZ,2)){
+        return true;
+    }else {
+        return false;
+    }
+    
 }
